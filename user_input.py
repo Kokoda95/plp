2ac9e3e91c3a24c32ba3be6b1c1b6ec677c03113
Kokoda95 <137652106+Kokoda95@users.noreply.github.com>
2024-02-25 22:46:07 +0100

    Add files via upload
    Add python file.

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# Ask for the details of the user
+name = input("Enter your name:")
+age = input("Enter your age:")
+location = input("Enter your location:")
+# Make the message using user inputs
+print(f"Hello! {name}, you are {age} years old and live in {location}.")
